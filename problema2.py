a3a034fe3719a6c126bf0cd38d887232dbd16594
Ednaldo Aquino <ednaldoaquino87@gmail.com>
2025-12-24 23:12:09 -0300

    adicionei linhas

--- problema2.py
+++ problema2.py
@@ -1,11 +1,13 @@
 #  Exercicio de verificação de Media 
 
 nome = input('Digite seu nome:')
+
 print('Olá,{}! Bem vindo ao sistema de verificação de média.'.format(nome))
 n1 = float(input('Digite sua Primeira Nota'))
 n2 = float(input('Digite sua Segunda Nota'))
 n3 = float(input('Digite sua terceira Nota'))
 media = (n1 + n2 + n3) / 3
+print()
 if media >= 7:
     print('Parabens! Voce foi Aprovado com a media:{:.2f}'.format(media))
 else:
