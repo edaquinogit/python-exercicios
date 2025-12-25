820962ee1f83e63e44393ef630dc7a1ba2d33fad
Ednaldo Aquino <ednaldoaquino87@gmail.com>
2025-12-25 00:38:47 -0300

    update verifica_idade.py
    mudança na organização do código

--- condicionais/verifica_idade.py
+++ condicionais/verifica_idade.py
@@ -1,7 +1,20 @@
 # Exercicio de verificação de idade
 
+print('Bem vindo ao sistema de verificação de idade!')
+
+print()
+
+nome = input('Digite seu nome:')
+print('Olá,{}!'.format(nome))
+
+print()
+
 n1 = float(input('Digite sua idade:'))
 if n1 >= 18:
    print('Acesso permitido! Voce é maior de idade.')
 else:
     print('Acesso negado!Voce é menor de idade')
+
+print()
+
+print('Obrigado por ultilizar noss o sistema de verificação de idade,{}!'.format(nome))
